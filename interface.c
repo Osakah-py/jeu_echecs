9b438189b45711e2af3ab95f78b41485dd49f74f
ALEX <lescot.alexg@gmail.com>
2022-10-17 11:57:05 +0200

    Unicode marche UwU

--- interface.c
+++ interface.c
@@ -1,9 +1,12 @@
+// https://linuxhint.com/unicode-c/
+
 # include <stdio.h>
 # include <string.h>
 # include <wchar.h>
 #include <locale.h>
 
 int main (int argc, char ** argv){
-    wprintf(L"U+2654 \n");
+    setlocale(LC_CTYPE, "");
+    wprintf(L"\x2654 \x265A \n");
     return 0;
 }
