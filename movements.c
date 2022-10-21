63dc6d86be2dcefcc945c1449ea57df8d0bbeecd
AceCoolDan123 <luodaniel2015@yahoo.fr>
2022-10-21 19:31:54 +0200

    movment in progress...

--- movements.c
+++ movements.c
@@ -4,13 +4,18 @@
 position = x
 horizontal pos = x % 8
 vertical pos = x / 8
+
+movement = {x, y, z}
+x = horizontal movement
+y = vertical movement
+z = can repeat this single movement ? (0 : nope, other value : yes)
 */
 
 
 // GLOBAL VARIABLES ------------------------------------------------------------------------------
 // a dictionnary of movement to each piece
-const char piece_key[6] = {'p'} 
-const int movmeent_value = 
+const char piece_key[6] = {'p', 'r', 'n', 'b', 'k', 'q'};
+//const int movement_value = {{1, 0}, {1, 0}}
 
 // PROTOTYPES ------------------------------------------------------------------------------------
 char check_movement(int position, char signature);
