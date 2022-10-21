25bb33da622baa66d79338d7066f84ce3807dbde
AceCoolDan123 <luodaniel2015@yahoo.fr>
2022-10-21 19:44:46 +0200

    dictionnary done

--- movements.c
+++ movements.c
@@ -5,17 +5,19 @@ position = x
 horizontal pos = x % 8
 vertical pos = x / 8
 
-movement = {x, y, z}
+movement = {x, y, z, a, b}
 x = horizontal movement
 y = vertical movement
 z = can repeat this single movement ? (0 : nope, other value : yes)
+a = multiple direction ? (0 : nope, other value : yes)
+nb : if x = 0 and y = 0 --> the piece can move in all directions ! 
 */
 
 
 // GLOBAL VARIABLES ------------------------------------------------------------------------------
 // a dictionnary of movement to each piece
-const char piece_key[6] = {'p', 'r', 'n', 'b', 'k', 'q'};
-//const int movement_value = {{1, 0}, {1, 0}}
+const char piece_key[6] = {       'p',          'r',          'n',          'b',          'k',          'q'};
+const int movement_value[6][6] = {{1, 0, 0, 0}, {1, 0, 1, 1}, {2, 1, 0, 1}, {1, 1, 1, 1}, {0, 0, 0, 1}, {0, 0, 1, 1}};
 
 // PROTOTYPES ------------------------------------------------------------------------------------
 char check_movement(int position, char signature);
