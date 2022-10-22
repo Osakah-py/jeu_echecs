4d0c0b7b5fcbccc109da82e02ecf966ba261e1cf
AceCoolDan123 <luodaniel2015@yahoo.fr>
2022-10-22 22:32:53 +0200

    movement in progress...

--- movements.c
+++ movements.c
@@ -1,4 +1,6 @@
 # include <stdio.h>
+# define Hpos(a) (a % 8) // l'abscisse de la position sur l'echequier
+# define Vpos(a) (a / 8) // l'ordonnee ...
 
 /* note :
 pos = x
@@ -10,7 +12,7 @@ x = mouvement horizontal
 y = mouvement vertical
 z = peut repeter ce mouvement ? (0 : nope, other value : yes)
 a =  son mouvement est multi-directionnel ? (0 : nope, autre valeur : yes)
-nb : if x = 0 et y = 0 --> la pièce peut bouger dans toutes les directions ! 
+nb : if x = 0 et y = 0 --> la piece peut bouger dans toutes les directions ! 
 */
 
 
@@ -19,12 +21,14 @@ nb : if x = 0 et y = 0 --> la pièce peut bouger dans toutes les directions !
 const int size_dict_movement = 6;
 const char piece_key[6] = {       'p',          'r',          'n',          'b',          'k',          'q'};
 const int movement_value[6][4] = {{1, 0, 0, 0}, {1, 0, 1, 1}, {2, 1, 0, 1}, {1, 1, 1, 1}, {0, 0, 0, 1}, {0, 0, 1, 1}};
+/* NB : pour la suite, le fait que les x y sont positives sera tres important !!!
+(surtout pour la fonction one_move) */
 
 
 // PROTOTYPES ------------------------------------------------------------------------------------
 int get_piece_key(const char signature);
-int init_directionvc(const int position, const int* mouvement);
-char check_movement(const int position, const char signature);
+int one_move(const int position, const int movement[4]); // -1
+int check_movement(const int position, const char signature); // faux --> 0 et vrai --> autres valeurs
 
 
 // FONCTIONS -------------------------------------------------------------------------------------
@@ -40,15 +44,49 @@ int get_piece_key(const char signature)
     return -1; // il n'a pas d'indice...
 }
 
-char check_movement(const int position, const char signature)
+int one_move(const int position, const destination, const int movement[4])
+{
+    int init_posH = posH(position);
+    int init_posV = posV(position);
+    int dest_posH = posH(destination);
+    int dest_posV = posV(destination);
+
+    if(movement[3] == 0)
+    {
+        if(init_posH < dest_posH && init_posV < dest_posV)
+        {
+            // il fait le mouvement brute (x et y toujours positifs dans le "dictionnaire"!!!)
+        }
+    }
+
+    return -1; // le mouvement ne peut pas aller au-dela de la destination    
+}
+
+int check_movement(const int position, const int destination, const char signature)
 {
     const int ind_key = get_piece_key(signature); // cherche le mouvement de la piece
-    if(movement_value[ind_key][2] == 0)
+    int movement[4] = movement_value[ind_key];
+    int inter_pos = position;
+
+    // Il faut check le cas ou le pion peut manger en diagonale (peut-etre une fonction a part)
+
+    if(movement[2] != 0)
     {
-        // verifie une seule fois le mouvement indique
+        // le mouvement est repete avec une boucle while
+        while(inter_pos != -1)
+        {
+            inter_pos = init_direction(inter_pos, destination, movement_value[ind_key]);
+        }
     }
     else
     {
-        //verifie si le mouvement est atteint avec une boucle while
+        // le mouvement est repete une seule fois
+        inter_pos = one_move(inter_pos, destination, movement);
     }
+    
+    if(inter_pos == destination)
+    {
+       return 1;
+    }
+    return 0;
 }
