e2aa2ebadd11f519cbee81d404bfa67d810ef99e
AceCoolDan123 <luodaniel2015@yahoo.fr>
2022-10-23 19:11:59 +0200

    movement changes

--- movements.c
+++ movements.c
@@ -29,9 +29,10 @@ const int movement_value[6][4] = {{1, 0, 0, 0}, {1, 0, 1, 1}, {2, 1, 0, 1}, {1, 
 
 // PROTOTYPES ------------------------------------------------------------------------------------
 int get_piece_key(const char signature);
-int check_
+int repeat_move(const int position, const int destination, const movement[2]);
+int multidirectionnal(int position, const int destination, const movement[2]);
 int direction_move(const int position, const int destination, const int movement[4]); // -1
-int check_movement(const int position, const int destination, const char signature); // faux --> 0 et vrai --> autres valeurs
+int check_movement(int position, const int destination, const char signature); // faux --> 0 et vrai --> autres valeurs
 
 
 // FONCTIONS -------------------------------------------------------------------------------------
@@ -39,7 +40,7 @@ int get_piece_key(const char signature)
 {
     for (int i = 0; i <size_dict_movement; i++)
     {
-        if(piece_key[i] == signature || toupper(piece_key[i]) == signature)
+        if (piece_key[i] == signature || toupper(piece_key[i]) == signature)
         {
             return i;
         }
@@ -47,89 +48,97 @@ int get_piece_key(const char signature)
     return -1; // il n'a pas d'indice...
 }
 
-int one_move(const int position, const int destination, const int movement[4])
+int repeat_move(const int position, const int destination, const movement[2])
 {
-    int init_posH = HPOS(position);
-    int init_posV = VPOS(position);
-    int dest_posH = HPOS(destination);
-    int dest_posV = VPOS(destination);
-
-    if ((init_posH + movement[0]) < dest_posH && (init_posV + movement[1] < dest_posV))
+    while(0)
     {
-            // la piece reste en bas a gauche de la destination
-        return (position + movement[0] + movement[1] * 8); 
-    }
 
-    // LE MOUVEMENT: x = movement[0] et y = movement[1] 
-    if (movement[3] != 0) // la pièce est multi-directionnelle
-    {
-        if ((init_posH - movement[0]) > dest_posH && (init_posV - movement[1] > dest_posV))
-        {
-            // la piece est en haut a droite de la destination
-            return (position - movement[0] - movement[1] * 8); 
-        }
-        if ((init_posH - movement[0]) > dest_posH && (init_posV + movement[0] < dest_posV))
-        {
-            // la piece est en bas a droite de la destination
-            return(position + movement[1] + movement[0] * 8);
-        }
-        if ((init_posH + movement[0]) < dest_posH && (init_posV + movement[1] < dest_posV))
-        {
-            //la piece est en haut a gauche de la destination
-            return(position - movement[0] + movement[1] * 8)
-        }
-        
     }
-    
-    // LE MOUVEMENT: x = movement[1] et x = movement[1] 
-    if (movement[3] != 0)
-    {
-        if ((init_posH - movement[0]) > dest_posH && (init_posV - movement[1] > dest_posV))
-        {
-            // la piece est en haut a droite de la destination
-            return (position - movement[0] - movement[1] * 8); 
-        }
-        if ((init_posH - movement[0]) > dest_posH && (init_posV + movement[0] < dest_posV))
-        {
-            // la piece est en bas a droite de la destination
-            return(position + movement[1] + movement[0] * 8);
-        }
-        if ((init_posH + movement[0]) < dest_posH && (init_posV + movement[1] < dest_posV))
-        {
-            //la piece est en haut a gauche de la destination
-            return(position - movement[0] + movement[1] * 8)
-        }
-        
-    }
-
-    return -1; // le mouvement ne peut pas aller au-dela de la destination    
 }
 
-int check_movement(int position, const int destination, const char signature)
+int multidirectionnal(const int position, const int destination, const int movement[3])
+{
+    const int init_posH = HPOS(position);
+    const int init_posV = VPOS(position);
+    const int dest_posH = HPOS(destination);
+    const int dest_posV = VPOS(destination);
+    if(movement[2] == 0)
+    {
+        return -1;
+    }
+    if (init_posH - movement[0] > dest_posH  &&  init_posV - movement[1] > dest_posV)
+    {
+        // la piece est en haut a droite de la destination
+        return (position - movement[0] - movement[1] * 8); 
+    }
+    if (init_posH - movement[0] > dest_posH  &&  init_posV + movement[1] < dest_posV)
+    {
+        // la piece est en bas a droite de la destination
+        return(position + movement[1] + movement[0] * 8);
+    }
+    if (init_posH + movement[0] < dest_posH  &&  init_posV - movement[1] > dest_posV)
+    {
+        //la piece est en haut a gauche de la destination
+        return(position - movement[0] + movement[1] * 8)
+    }
+    if (init_posH + movement[0] < dest_posH  &&  init_posV + movement[1] < dest_posV)
+    {
+        //la piece est en bas a gauche de la destination
+        return (position - movement[0] + movement[1] * 8)
+    }
+    return -1;
+}
+
+int direction_move(int position, const int destination, const int movement[4])
+{
+    int movement_adjusted[4] = movement; 
+    position = multidirectionnal(position, destination, movement_default);
+    if (movement[3] != 0 && position == -1) // la pièce est multi-directionnelle
+    {
+        //on ajuste movement pour l'adapter a multidirectionnal
+    movement_adjusted[0] = movement[1];
+    movement_adjusted[1] = movement[0]; // on inverse pour avoir toutes les possibilites
+    multidirectionnal(position, destination, movement_default);
+    }
+    return position; // le mouvement ne peut pas aller au-dela de la destination    
+}
+
+
+
+int check_movement(int position, const int destination, const char signature, char tableau[8][8])
 {
     const int ind_key = get_piece_key(signature); // cherche le mouvement de la piece
-    int inter_pos = position;
+    const char target = tableau[HPOS(destination)][VPOS(destination)];
 
     // Il faut check le cas ou le pion peut manger en diagonale (peut-etre une fonction a part)
-
-    if(movement_value[ind_key][2] != 0)
+    if ((signature = 'p' || signature = 'P') 
+        && (position-1 + 1*8 == destination || position+1 + 1*8 == destination) )
     {
-        // le mouvement est repete avec une boucle while
-        while(inter_pos != -1)
+        if(target == '0')
         {
-            position = inter_pos;
-            inter_pos = one_move(position, destination, movement_value[ind_key]);
+            return 0; // un pion ne mange pas s'il y a rien :D
         }
+        position = destination;
     }
     else
     {
-        // le mouvement est repete une seule fois
-        inter_pos = one_move(inter_pos, destination, movement_value[ind_key]);
+        position = direction_move(position, destination, movement_value[ind_key])
     }
-    
-    if(inter_pos == destination)
+
+    if (position == destination)
     {
-       return 1; // la piece est bien arrivee à son destination prevue
+        if (target == '0')
+        {
+            return 1;
+        }
+        // les deux pieces ne sont pas de meme couleur
+        else if ( isupper(signature) && !isupper(target)
+                || !isupper(signature) && isupper(target) )
+        {
+            // noter la piece mangee 
+            return 1;            
+        }
     }
+
     return 0;
 }
