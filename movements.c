e3d609cddcb97afa7b8301ead71772002b04b8c0
AceCoolDan123 <luodaniel2015@yahoo.fr>
2022-10-24 23:31:50 +0200

    big changes in movement.c

--- movements.c
+++ movements.c
@@ -54,8 +54,10 @@ int diagonale_cond(const int position, const int destination, const char signatu
 int find_final_pos_pawn(const int position, const int destination, const char signature, const char target);
 // renvoie 0 si le mouvement n'est pas valide, sinon 1
 int check_movement(int position, const int destination, const char signature, const char tableau[8][8]); // faux --> 0 et vrai --> autres valeurs
-
-int is_check(const int position, const int movement[4]);
+void upper_enemies_or_not(const struct piece target, char *enemies, const int size);
+int check_moveset(const struct piece target, const int movX, const int movY, char *enemies, const int nb_enemies, const int range);
+int is_king_safe(const struct piece king);
+//int is_check(const int position, const int movement[4]);
 
 
 // FONCTIONS -------------------------------------------------------------------------------------
@@ -296,19 +298,98 @@ int check_movement(int position, const int destination, const char signature, co
 
 // IS CHECK ---------------------------------------------------------------------------------- 
 
-// On suppose qu'il est appele apres check_movement, et donc chessboard_mv est bien actualise
-int is_king_safe(int is_white_king)
+// on suppose que les characteres dans enemies sont en miniscule
+void upper_enemies_or_not(const struct piece target, char *enemies, const int size)
 {
-    if(is_white_king)
-    {        
-        for (int i = 0; i < 8; i ++)
+    if(!isupper(target.signature))
+    {
+        for (int i = 0; i < size; i++)
         {
-            if(!isupper(chessboard_mv[i][wKing.posX]) )
+            enemies[i] = toupper(enemies[i]);
+        }
+        
+    }
+}
+
+// pour un mouvement en particulier a partir du roi (0 si le roi est en echec)
+int check_moveset(const struct piece target, const int movX, const int movY, char *enemies, const int nb_enemies, const int range)
+{
+    int posX; int posY; 
+    int movX_bis; int movY_bis; 
+    //On ajuste enemies par rapport a target
+    upper_enemies_or_not(target, enemies, nb_enemies);
+    // On a tout les combinaisons possibles avec movX et movY
+    const int move_combo[8][2] = 
+    {{movX, movY}, {-movX, -movY}, {movY, movX}, {-movY, -movX},
+    {-movX, movY}, {movX, -movY}, {-movY, movX}, {movY, -movX}};
+
+    register char element = '0'; 
+    // on part de la cible pour verifier si un ennemi le menace avec movX et movY 
+    for (register int a = 0; a < 8; a++)
+    {
+        posX = target.posX;
+        posY = target.posY;
+        movX_bis = move_combo[a][0];
+        movY_bis = move_combo[a][1];
+        
+        // on check jusqu'a la portee desiree
+        for (register int j = 0; j < range; j++)
+        {    
+            posX += movX_bis;
+            posY += movY_bis;        
+
+            if(posX < 8 && posY < 8 && posX >= 0 && posY >= 0) // on regarde si on sort de l'echiquier
             {
-                
+                element = chessboard_mv[posX][posY];
+                for (register int i = 0; i < nb_enemies; i++)
+                {
+                    if(element == enemies[i])
+                    {
+                        return 0;
+                    }
+                }
+            }
+            else
+            {
+                break; // ca sort de l'echiquier donc on arrete la boucle
             }
         }
     }
+    return 1;
+}
+
+
+
+// On suppose qu'il est appele apres check_movement, et donc chessboard_mv est bien actualise
+int is_king_safe(const struct piece king)
+{
+    int tmp = 0;
+    char enemies2[2] = {'0','0'};
+    char enemy[1] = {'0'};
+    
+    // On regarde les pieces qui peuvent atteindre horizontalement et verticalament le roi    
+    enemies2[0] = 'r';
+    enemies2[1] = 'q';
+    tmp = check_moveset(king, 1, 0, enemies2, 2, 8);
+    if(tmp == 0)
+    {
+        return 0;
+    }  
+    // On regarde les pieces qui peuvent atteindre diagonalement le roi    
+    enemies2[0] = 'b';
+    tmp = check_moveset(king, 1, 1, enemies2, 2, 8);
+    if(tmp == 0)
+    {
+        return 0;
+    }
+    // On regarde les cavaliers qui peuvent atteindre le roi    
+    enemy[0] = 'n';
+    tmp = check_moveset(king, 1, 1, enemy, 2, 8);
+    if(tmp == 0)
+    {
+        return 0;
+    }
+    return 1;
 }
 /*
 int is_check()
