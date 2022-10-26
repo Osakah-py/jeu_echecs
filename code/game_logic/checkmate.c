8c18cd45222637492a0d871af13bdc815d10c0d5
AceCoolDan123 <luodaniel2015@yahoo.fr>
2022-10-26 19:13:36 +0200

    delete struct piece which is useless

--- code/game_logic/checkmate.c
+++ code/game_logic/checkmate.c
@@ -1,20 +1,27 @@
 # include "data.h"
 # include "movements.h"
 # include "check.h"
+# include "piece_manager.h"
 # include <stdio.h>
 
 // VARAIBLES GLOBALES --------------------------------------------------------------
-extern piece pieces_logic[64];
 extern char chessboard_logic[8][8];
-extern piece wKing;
-extern piece bKing;
 
+
+
+
+// On veut savoir si on peut manger la piece ennemi pour eviter un echec et mat
+int can_eat_enemy(const int enemy_position)
+{
+
+}
 
 // 0 : le roi ne peut pas se deplacer autour de lui, sinon 1
-int can_move_around(piece king)
+int can_move_around(const int position, const char signature)
 {
-    int init_posX = king.posX;
-    int init_posY = king.posY;
+    int target_position;
+    int init_posX = HPOS(position);
+    int init_posY = VPOS(position);
     int modif[3] = {0, 1, -1}; // on veut avoir toutes les modifs pour avoir les 8 cases autour du roi
     //int cond;
 
@@ -26,10 +33,10 @@ int can_move_around(piece king)
             { 
                 continue;
             }
-            king.posX = init_posX + modif[i];
-            king.posY = init_posY + modif[j];
+            target_position = init_posX + modif[i];
+            target_position = init_posY + modif[j];
             //le roi peut bouger sur une case sans danger ?
-            if(chessboard_logic[king.posY][king.posX] == '0' && is_king_safe(king)) 
+            if(chessboard_logic[target_position][target_position] == '0' && is_king_safe(signature)) 
             {
                 return 1;
             }
@@ -40,14 +47,25 @@ int can_move_around(piece king)
 }
 
 
-
-// 0 si ce n'est pas un echec et mat
-int checkmate(int king_is_white)
+// On suppose que le roi est deja en echec et la fct renvoie 0 si ce n'est pas un echec et mat 
+int is_checkmate(const int is_king_white, const int pos_enemy)
 {
-    if(can_move_around(wKing))   
+    int king_position;
+    char king_signature;
+    if(is_king_white)
+    {
+        king_signature = "R";
+    }
+    else
+    {
+        king_signature = 'r';
+    }
+    king_position = find_king_pos(king_signature);
+    if(can_move_around(king_position, king_signature))   
     {
         return 0;
     }
+    if()
 
     return 1;
 }
