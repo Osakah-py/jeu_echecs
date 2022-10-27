c0b5fb4bf326c5fe8805db0e4280587059f117be
Dadidou <luodaniel2015@yahoo.fr>
2022-10-27 22:47:24 +0200

    Add files via upload

---
+++ headers/logic/special_movements.h
@@ -0,0 +1,9 @@
+#ifndef special_movements
+#define special_movements
+
+#include <stdio.h>
+
+int special_mvt_controller(const int position, const int destination, const char signature, const char target);
+
+
+#endif
