338a5c8f8e7dffc36ec19d3722bf6471662493f8
AceCoolDan123 <luodaniel2015@yahoo.fr>
2022-10-28 22:34:03 +0200

    checkmate bug fixed

--- headers/logic/logic_data.h
+++ headers/logic/logic_data.h
@@ -7,8 +7,11 @@
 # define VPOS(a) (a) / 8 // l'ordonnee ...
 
 extern int pos_enemy;
+extern int mvt_enemy[2];
+
 extern int two_moves_in_once;
 extern int has_wking_moved;
 extern int has_bking_moved;
+
 
 #endif
--- headers/logic/movements.h
+++ headers/logic/movements.h
@@ -2,7 +2,7 @@
 #define movements
 # include <ctype.h>
 # include <stdio.h>
- 
+
 // faux --> 0 et vrai --> autres valeurs
 int is_movement_correct(const int position, const int destination); 
 
